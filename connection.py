2d47efa0fc6bd14c3c8a339141916fd8112d6108
Tkad134 <5sai.kad@gmail.com>
2025-10-12 21:34:53 -0700

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,17 +1,17 @@
-# Import the 'connection' class from the OpenStack SDK
-# This class provides a high-level interface to interact with OpenStack services (e.g., compute, network, identity)
+# Import the connection class from the OpenStack SDK
+# This class provides a high-level interface to interact with OpenStack services 
 from openstack import connection
 
 # Define a function that creates and returns an authenticated OpenStack connection object
 def create_connection():
     # Return a new OpenStack connection using the provided authentication parameters
-    # These credentials must match what’s configured on your OpenStack environment
+    # Match these credentials to what you have configured on your openstack environment. 
     return connection.Connection(
         # The URL of the OpenStack Identity (Keystone) service
         # This is where authentication requests are sent
         auth_url='insert url here',
 
-        # The name of the OpenStack project (tenant) to authenticate into
+        # The name of the OpenStack project to authenticate into
         project_name='admin',
 
         # The username used to log in to the OpenStack environment
@@ -20,9 +20,9 @@ def create_connection():
         # The corresponding password for the username above
         password='insert password here',
 
-        # The domain name of the user (commonly "Default" in DevStack)
+        # The domain name of the user 
         user_domain_name='Default',
 
-        # The domain name of the project (commonly "Default" in DevStack)
+        # The domain name of the project 
         project_domain_name='Default'
     )
